8369b16ad7d1f3580771aadb6bfacedbd0def166
humanity labs <humanitylabs@protonmail.com>
2023-10-08 08:35:19 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# static-thing
+
+> Minimal static [SvelteKit](https://kit.svelte.dev/) website deployabled to [GitHub Pages](https://humanitylabs.github.io/static-thing/).
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1>static-thing</h1>
+<p>Deployed to GitHub Pages</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<a href="{base}/thing">thing</a>
